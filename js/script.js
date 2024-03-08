846f5faca1a017260489074498ee4395297a4e8a
cyber-mannu <sahanimannu844@gmail.com>
2024-03-08 21:21:31 +0530

    fix anchor tag animation

--- js/script.js
+++ js/script.js
@@ -35,6 +35,24 @@ function loco() {
 
 loco();
 
+document.querySelectorAll("a").forEach((link) => {
+  link.addEventListener("mouseover", () => {
+    const text = new SplitType(link, { types: "words, chars" });
+
+    gsap.from(text.chars, {
+      duration: 0.6,
+      // transformOrigin: "10% 50%",
+      ease: "back",
+      stagger: 0.02,
+      scale: 2,
+      autoAlpha: 0,
+      rotationX: -80,
+      // y: 10,
+      x: 10,
+    });
+  });
+});
+
 Shery.textAnimate(" .page1 h1, .page1 p", {
   style: 1,
   y: 50,
@@ -65,50 +83,6 @@ gsap.to(".content-anim h1 span", {
   color: "#741BD4",
 });
 
-// let para = " ";
-// document
-//   .querySelector(".wrapper2 h1")
-//   .textContent.split(" ")
-//   .forEach(function (dets) {
-//     paragraph += `<span> ${dets} </span>`;
-//     document.querySelector(".wrapper2 h1, #nd2").innerHTML = paragraph;
-//   });
-
-//   gsap.to(".wrapper2 h1 span ", {
-//     scrollTrigger: {
-//       trigger: ".wrapper2 h1 span ",
-//       scroller: ".main",
-//       start: "top 80%",
-//       end: "top 20%",
-
-//       scrub: 0.5,
-//     },
-//     stagger: 0.2,
-//     color: "#741BD4",
-//   });
-
-//   let para2 = " ";
-// document
-//   .querySelector(" #nd2")
-//   .textContent.split(" ")
-//   .forEach(function (dets) {
-//     paragraph += `<span> ${dets} </span>`;
-//     document.querySelector("#nd2").innerHTML = paragraph;
-//   });
-
-//   gsap.to(" #nd2 span", {
-//     scrollTrigger: {
-//       trigger: " #nd2 span",
-//       scroller: ".main",
-//       start: "top 80%",
-//       end: "top 20%",
-// markers:true,
-//       scrub: 0.5,
-//     },
-//     stagger: 0.2,
-//     color: "#741BD4",
-//   });
-
 const text = new SplitType(".wrapper2 h1 ", { types: "words, chars" });
 
 gsap.from(text.chars, {
@@ -126,13 +100,12 @@ gsap.from(text.chars, {
   color: "#232323",
 });
 
-gsap.to('.page5 img', {
-  scrollTrigger:{
-    start:'top 10%',
-    end:'top -150%',
-    markers:true,
-scroller:'.main',
-trigger:'.page5 img',
-pin:true,
-  }
-})
+gsap.to(".page5 img", {
+  scrollTrigger: {
+    start: "top 10%",
+    end: "top -150%",
+    scroller: ".main",
+    trigger: ".page5 img",
+    pin: true,
+  },
+});
